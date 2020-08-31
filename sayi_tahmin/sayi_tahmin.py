1d2550a5277dbd66e30bf7bdca4b1e00a7153d1d
githsem <53379599+githsem@users.noreply.github.com>
2020-08-31 19:51:37 +0200

    update

---
+++ sayi_tahmin/sayi_tahmin.py
@@ -0,0 +1,45 @@
+import random
+import time
+
+print("""
+****************************************
+
+           Sayi Tahmin Oyunu
+           -----------------
+    
+1 ile 40 arasinda bir sayi tahmin ediniz    
+
+
+****************************************
+""")
+
+
+sayi = random.randint(1,40)
+hak=7
+
+while True:
+    tahmin = int(input("Tahmininiz : "))
+
+    if tahmin < sayi:
+        print("Bilgiler Sorgulaniyor...")
+        time.sleep(1)
+        print("Daha buyuk bir sayi giriniz...")
+        hak-=1
+    elif tahmin > sayi:
+        print("Bilgiler Sorgulaniyor...")
+        time.sleep(1)
+        print("Daha kucuk bir sayi giriniz...")
+        hak-=1
+    else :
+        print("Bilgiler Sorgulaniyor",end=" .")
+        time.sleep(1)
+        print(".",end=" ")
+        time.sleep(1)
+        print(".", end="\n")
+        time.sleep(1)
+        print("Tebrikler Dogru Tahmin Ettiniz")
+        break
+
+    if hak==0:
+        print()
+        break
