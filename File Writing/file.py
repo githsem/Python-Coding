15e79a4acbebfd98b5c00fc98a01fa5128af803a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-03 00:22:21 +0200

    new file

--- File Writing/file.py
+++ File Writing/file.py
@@ -1,3 +1,6 @@
-file = open("bilgi.txt",w)
-file.write("Merhabalar")
+file = open("bilgi.txt","w")
+file.write("Merhabalar\n")
+file.close()
+file = open("bilgi.txt","a")
+file.write("Nasilsiniz")
 file.close()
