6432875a7254912a5f9eb9a78c158e40e408e21a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-03 01:34:05 +0200

    Update read.py

--- File Writing/read.py
+++ File Writing/read.py
@@ -3,3 +3,9 @@ file = open("bilgi.txt","r",encoding="UTF-8")
 for i in file:
     print(i,end="")
 file.close()
+print("**********************************")
+file = open("bilgi.txt","r",encoding="UTF-8")
+icerik = file.read()
+print(icerik)
+file.close()
+
