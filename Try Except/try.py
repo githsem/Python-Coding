0c5a7fa743c9934367bf9ef5154235a2da12a9f2
githsem <53379599+githsem@users.noreply.github.com>
2020-09-02 23:58:22 +0200

    Update try.py

--- Try Except/try.py
+++ Try Except/try.py
@@ -4,3 +4,9 @@ def tek_cift(sayi):
     else:
         raise ValueError("Tek Sayi Hatasi")
 
+liste =[1,2,3,4,5,6,7,8,9,10]
+for i in liste:
+    try:
+        print(tek_cift(i))
+    except:
+        print("Tek Sayi")
