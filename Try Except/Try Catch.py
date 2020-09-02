01567bd4350b6c985fe1571fe21c93e52a6fc19c
githsem <53379599+githsem@users.noreply.github.com>
2020-09-02 23:40:45 +0200

    Update Try Catch.py

---
+++ Try Except/Try Catch.py
@@ -0,0 +1,9 @@
+liste = ["123","wer","23we","344","ss"]
+
+for i in range(0,5):
+    try:
+        if liste[i] == int(liste[i]):
+            print(liste[i])
+    except:
+        print("hata")
+        print(liste[i])
