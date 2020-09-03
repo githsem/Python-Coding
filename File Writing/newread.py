3a6fba56dec168ac8582d3b14da25f7eb2b62c53
githsem <53379599+githsem@users.noreply.github.com>
2020-09-03 13:56:45 +0200

    update

--- File Writing/newread.py
+++ File Writing/newread.py
@@ -6,3 +6,7 @@ with open("bilgi.txt","r",encoding ="UTf-8") as file:
     print(file.tell())
     file.seek(12)
     print(file.tell())
+
+with open("bilgi.txt","r",encoding ="UTf-8") as file:
+    file.seek(2)
+    print(file.read(5))
