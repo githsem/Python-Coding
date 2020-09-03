535d3b22e7fb163583a099d4a58b8213093cb5c0
githsem <53379599+githsem@users.noreply.github.com>
2020-09-03 15:18:36 +0200

    Update ogrenci not.py

--- File Writing/ogrenci not.py
+++ File Writing/ogrenci not.py
@@ -1,4 +1,7 @@
+def not_hesapla(satir):
+    satir = satir[:-1]
+
+
 with open("sinav.txt", "r", encoding ="UTf-8") as file:
    for i in file:
-       print(i)
-       print(1)
+       not_hesapla(i)
