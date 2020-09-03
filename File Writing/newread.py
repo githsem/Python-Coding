0e0a2cce1abe366ffc03d69da8a3182c6acdbc6b
githsem <53379599+githsem@users.noreply.github.com>
2020-09-03 13:48:34 +0200

    file seek-tell

--- File Writing/newread.py
+++ File Writing/newread.py
@@ -1,3 +1,8 @@
 with open("bilgi.txt","r",encoding ="UTf-8") as file:
     for i in file:
         print(i)
+
+with open("bilgi.txt","r",encoding ="UTf-8") as file:
+    print(file.tell())
+    file.seek(12)
+    print(file.tell())
