22e39662cc06b27f8a15432b7c49ca1c55c0437d
githsem <53379599+githsem@users.noreply.github.com>
2020-09-04 18:26:21 +0200

    answers

---
+++ Gomulu Fonksiyonlar/answer 1.py
@@ -0,0 +1,5 @@
+liste = [(3,4),(10,3),(5,6),(1,9)]
+
+def alan(demet):
+    return demet[0]*demet[1]
+print(list(map(alan,liste)))
---
+++ Gomulu Fonksiyonlar/answer 2.py
@@ -0,0 +1,4 @@
+def ucgen(demet)
+    if (demet[0]+demet[1])>demet[2]>abs(demet[1]+demet[2])
+        return demet
+print()
