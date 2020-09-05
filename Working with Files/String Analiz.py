cb8f01956f3411bc4435cf7bc9c0970186efdbde
githsem <53379599+githsem@users.noreply.github.com>
2020-09-05 16:14:26 +0200

    new analiz

---
+++ Working with Files/String Analiz.py
@@ -0,0 +1,11 @@
+string = "ProgramlamaÖdeviİleriSeviyeVeriYapılarıveObjeleripynb"
+
+harfler = list(string)
+harf_sozluk = dict()
+
+for i in list:
+    if i in harf_sozluk:
+        harf_sozluk[i] += 1
+    else:
+        harf_sozluk.
+        harf_sozluk[i] = 1
