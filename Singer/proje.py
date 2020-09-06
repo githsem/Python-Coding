09772e3bc920c1c3c1009f11a40a3ee63871fa00
githsem <53379599+githsem@users.noreply.github.com>
2020-09-07 01:29:34 +0200

    update

--- Singer/proje.py
+++ Singer/proje.py
@@ -7,14 +7,35 @@ print("""
 
 İşlemler;
 
-1. Sarkilari Göster
+1. Sarki Ekle
 
-3. Sarki Ekle
+2. Sarki Sil 
 
-4. Sarki Sil 
-
-5. Sure Hesapla
+3. Sure Hesapla
 
 Çıkmak için 'q' ya basın.
 ***************************************************""")
 sarkilar = Sarkilar()
+
+while True:
+    işlem = input("Yapacağınız İşlem:")
+
+    if (işlem == "q"):
+        print("Program Sonlandırılıyor.....")
+        print("Yine bekleriz....")
+        break
+    elif (işlem == "1"):
+        isim = input("İsim:")
+        sanatci = input("Sanatci:")
+        album = input("Album:")
+        sirket = input("Sirket:")
+        sure = int(input("Sure:"))
+
+        yeni_sarki = Sarki(isim, sanatci, album, sirket, sure)
+
+        print("Sarki ekleniyor....")
+        time.sleep(2)
+
+        sarkilar.sarki_ekle(yeni_sarki)
+        print("Sarki Eklendi....")
+
