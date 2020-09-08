56aa1e30de5483eac46b6f4453c461f8aa48bf9d
githsem <53379599+githsem@users.noreply.github.com>
2020-09-08 16:41:34 +0200

    Update Decorator.py

--- Decorator Time/Decorator.py
+++ Decorator Time/Decorator.py
@@ -1,3 +1,20 @@
+def extra(func):
+    def wrapper(sayilar):
+        tek_sayi = 0
+        tek_sayilar = 0
+        cift_sayi = 0
+        cift_sayilar = 0
+        for sayi in sayilar:
+            if sayi%2==0:
+                cift_sayi += 1
+                cift_sayilar +=sayi
+            else:
+                tek_sayi += 1
+                tek_sayilar += sayi
+        print("Tek sayilar orta")
+
+
+
 
 def ortalama_hesapla(sayilar):
     toplam = 0
