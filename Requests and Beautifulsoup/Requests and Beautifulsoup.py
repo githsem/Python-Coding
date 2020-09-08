f0740f10b9edc5333081ee140c552fd653ffd533
githsem <53379599+githsem@users.noreply.github.com>
2020-09-09 01:30:18 +0200

    Update Requests and Beautifulsoup.py

--- Requests and Beautifulsoup/Requests and Beautifulsoup.py
+++ Requests and Beautifulsoup/Requests and Beautifulsoup.py
@@ -2,10 +2,11 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = "https://yellowpages.com.tr/ara?q=ankara"
+url = "https://www.n11.com/"
 response = requests.get(url)
 html_icerigi = response.content
 
 soup = BeautifulSoup(html_icerigi,"html.parser")
 
-print(soup.find_all("div",{"class":"yp-poi-box-2" }))
+for i in soup.find_all("a"):
+    print(i.get("href"))
