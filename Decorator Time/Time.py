dcf21d546b2ab617435e610dbf6077448ef4dc7a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-08 02:05:40 +0200

    update

--- Decorator Time/Time.py
+++ Decorator Time/Time.py
@@ -1,7 +1,22 @@
 import time
+def zaman_hesapla(func):
+    def wrapper(sayilar):
+        baslama = time.time()
+        sonuc = func()
+        bitis = time.time()
+        print(func.__name__+ " " + str(bitis-baslama) + "saniye surdu")
+
 
 
 def kare_hesapla(sayilar):
+    sonuc =[]
     for i in sayilar:
-        sonuc = i**2
-        return sonuc
+        sonuc.append(i**2)
+    return sonuc
+def kup_hesapla(sayilar):
+    sonuc =[]
+    for i in sayilar:
+        sonuc.append(i**3)
+    return sonuc
+
+kare_hesapla(range(100))
