912ebd1f7655ea8f167b669cb9edb1bb4d110836
githsem <53379599+githsem@users.noreply.github.com>
2020-09-09 16:58:52 +0200

    update

--- Requests and Beautifulsoup/imdb.py
+++ Requests and Beautifulsoup/imdb.py
@@ -9,16 +9,19 @@ soup = BeautifulSoup(html_icerigi,"html.parser")
 basliklar = soup.find_all("td",{"class":"titleColumn"})
 ratingler = soup.find_all("td",{"class":"ratingColumn imdbRating"})
 
+a= float(input("Rating Giriniz :"))
 for baslik,rating in zip(basliklar,ratingler):
     baslik = baslik.text
     rating = rating.text
 
     baslik = baslik.strip()
     baslik = baslik.replace("\n","")
+    baslik = baslik.strip("0123456789. ")
 
     rating = rating.strip()
     rating = rating.replace("\n","")
 
-    print("Film Adi : ",baslik)
-    print("Rating : ",rating)
-    print("***********************************************************")
+    if float(rating)>a:
+        print("Film Adi : ",baslik)
+        print("Rating : ",rating)
+        print("***********************************************************")
