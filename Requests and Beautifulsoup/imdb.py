fd7b018adec23397d3623bf02200e1507d989e68
githsem <53379599+githsem@users.noreply.github.com>
2020-09-09 16:38:56 +0200

    Update imdb.py

--- Requests and Beautifulsoup/imdb.py
+++ Requests and Beautifulsoup/imdb.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+response = requests.get(url)
+html_icerigi = response.content
+soup = BeautifulSoup(html_icerigi,"html.parser")
+
+basliklar = soup.find_all("td",{"class : titleColumn"})
+rating =
