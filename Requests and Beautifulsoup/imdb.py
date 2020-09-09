5bf6f1b19198e6ac7bde3b69852e8d5425e69ad3
githsem <53379599+githsem@users.noreply.github.com>
2020-09-09 16:49:38 +0200

    Update imdb.py

--- Requests and Beautifulsoup/imdb.py
+++ Requests and Beautifulsoup/imdb.py
@@ -7,5 +7,16 @@ html_icerigi = response.content
 soup = BeautifulSoup(html_icerigi,"html.parser")
 
 basliklar = soup.find_all("td",{"class":"titleColumn"})
-rating = soup.find_all("td",{"class":"ratingColumn imdbRating"})
+ratingler = soup.find_all("td",{"class":"ratingColumn imdbRating"})
 
+for baslik,rating in zip(basliklar,ratingler):
+    baslik = baslik.text
+    rating = rating.text
+
+    baslik = baslik.strip()
+    baslik = baslik.replace("\n","")
+
+    rating = rating.strip()
+    rating = rating.replace("\n","")
+
+    print(baslik,rating)
