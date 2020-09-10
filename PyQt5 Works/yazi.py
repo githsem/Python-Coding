4b93ecf81cffe43ab375eae77159638dd1272de6
githsem <53379599+githsem@users.noreply.github.com>
2020-09-10 19:58:25 +0200

    Create yazi.py

---
+++ PyQt5 Works/yazi.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import QtWidgets
+
+def pencere():
+    app = QtWidgets.QApplication(sys.argv)
+    pencere = QtWidgets.QWidget()
+    pencere.setWindowTitle("Ders 2")
+    etiket = QtWidgets.QLabel(pencere)
+    etiket.setText("Burasi bir yazidir")
+
+
+    pencere.setGeometry(500,200,400,400)
+    pencere.show()
+    sys.exit(app.exec_())
+
+pencere()
