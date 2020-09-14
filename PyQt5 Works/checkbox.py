d24792cc734afd40b9431c08ed3aab5d82da66b1
githsem <53379599+githsem@users.noreply.github.com>
2020-09-14 02:01:51 +0200

    Update checkbox.py

--- PyQt5 Works/checkbox.py
+++ PyQt5 Works/checkbox.py
@@ -3,3 +3,20 @@ import sys
 from PyQt5.QtWidgets import QWidget,QApplication,QCheckBox,QLabel,QPushButton,QVBoxLayout
 
 
+class Pencere(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.checkbox =QCheckBox("Python'i Seviyor musunuz ?")
+        self.yazi_alani = QLabel("")
+        self.buton = QPushButton("Tikla")
+
+        v_box = QVBoxLayout()
+
+        v_box.addWidget(self.checkbox)
+        v_box.addWidget(self.yazi_alani)
+        v_box.addWidget(self.buton)
+
+        
