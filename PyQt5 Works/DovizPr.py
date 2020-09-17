74ce550ce0d9f2c9f8f310f2c276458b826c4c8a
githsem <53379599+githsem@users.noreply.github.com>
2020-09-18 00:07:24 +0200

    Update DovizPr.py

--- PyQt5 Works/DovizPr.py
+++ PyQt5 Works/DovizPr.py
@@ -99,7 +99,7 @@ class Ui_Doviz(object):
         soup = BeautifulSoup(html_icerigi, "html.parser")
         isim = soup.find_all("span", {"class": "name"})
         kur = soup.find_all("span", {"class": "value"})
-        
+
         for i, j in zip(isim, kur):
             i = i.text
             j = j.text
