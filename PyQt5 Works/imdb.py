eff018375ea7ddb0213b2a9d4e9d0e751e04515f
githsem <53379599+githsem@users.noreply.github.com>
2020-09-18 00:26:09 +0200

    Update imdb.py

--- PyQt5 Works/imdb.py
+++ PyQt5 Works/imdb.py
@@ -55,20 +55,24 @@ class Ui_Form(object):
 
     def veri_cek(self):
         url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+        
         response = requests.get(url)
         html_icerigi = response.content
         soup = BeautifulSoup(html_icerigi, "html.parser")
 
         basliklar = soup.find_all("td", {"class": "titleColumn"})
         ratingler = soup.find_all("td", {"class": "ratingColumn imdbRating"})
-        
+
         sayi = float(self.lineEdit.text())
+
         for baslik, rating in zip(basliklar, ratingler):
             baslik = baslik.text
             rating = rating.text
+
             baslik = baslik.strip()
             baslik = baslik.replace("\n", "")
             baslik = baslik.strip("0123456789. ")
+
             rating = rating.strip()
             rating = rating.replace("\n", "")
             if float(rating) > sayi:
