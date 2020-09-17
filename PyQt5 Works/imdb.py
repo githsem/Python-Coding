58500cf9ad3b52e74f3d373dab0db08760c16e34
githsem <53379599+githsem@users.noreply.github.com>
2020-09-18 00:20:28 +0200

    Update imdb.py

--- PyQt5 Works/imdb.py
+++ PyQt5 Works/imdb.py
@@ -61,7 +61,6 @@ class Ui_Form(object):
 
         basliklar = soup.find_all("td", {"class": "titleColumn"})
         ratingler = soup.find_all("td", {"class": "ratingColumn imdbRating"})
-
         sayi = float(self.lineEdit.text())
         for baslik, rating in zip(basliklar, ratingler):
             baslik = baslik.text
