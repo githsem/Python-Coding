721db18e7cdf031fdc670eb94006506759838ad6
githsem <53379599+githsem@users.noreply.github.com>
2020-11-23 00:45:32 +0100

    Update iterator.py

--- Iterator and Generator/iterator.py
+++ Iterator and Generator/iterator.py
@@ -5,7 +5,7 @@ class Kuvvet3():
     def __iter__(self):
         return self
     def __next__(self):
-        if (self.kuvvette < self.max):
+        if (self.kuvvet < self.max):
             sonuc = 3 ** self.kuvvet
             self.kuvvet +=1
             return sonuc
